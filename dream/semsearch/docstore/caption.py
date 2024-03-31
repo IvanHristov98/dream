69004050f4647cb2b569d2185f8ea7bd8f27133a
Ivan Hristov <ivan.hristov@siman.tech>
2024-03-31 16:44:12 +0300

    Implement captions store

--- dream/semsearch/docstore/caption.py
+++ dream/semsearch/docstore/caption.py
@@ -1,11 +1,36 @@
 from typing import List
 
+from sentence_transformers import SentenceTransformer
+
 import dream.voctree.api as vtapi
+import dream.pg as dreampg
+from dream import model
+from dream.semsearch.docstore.im_metadata import sample_im_metadata
 
 
 class CaptionStore(vtapi.DocStore):
+    _DIM_SIZE = 384
+
+    _transformer: SentenceTransformer
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._transformer = SentenceTransformer("paraphrase-MiniLM-L6-v2")
+
     def get_documents(self, tx: any, sample_size: int) -> List[vtapi.Document]:
-        raise NotImplementedError("")
+        pg_tx = dreampg.to_tx(tx)
+
+        ims: List[model.Image] = sample_im_metadata(pg_tx, sample_size)
+        docs: List[vtapi.Document] = []
+
+        for im in ims:
+            vectors = self._transformer.encode(im.captions)
+            doc = vtapi.Document(im.id, vectors)
+            
+            docs.append(doc)
+    
+        return docs
 
     def get_feature_dim(self) -> int:
-        raise NotImplementedError("")
+        return self._DIM_SIZE
