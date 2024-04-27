3d83b521476f1c50601964582147482d8d6c86d5
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2024-04-27 09:58:58 +0300

    Serve images (#7)

--- dream/controller/restapi/__init__.py
+++ dream/controller/restapi/__init__.py
@@ -1,2 +1,2 @@
 from dream.controller.restapi.train import register_train_endpoint  # noqa
-from dream.controller.restapi.semsearch import register_images_endpoint  # noqa
+from dream.controller.restapi.semsearch import register_images_endpoint, register_image_endpoint  # noqa
--- dream/controller/restapi/semsearch.py
+++ dream/controller/restapi/semsearch.py
@@ -11,7 +11,7 @@ _INCLUDE_UNLABELED = "include_unlabeled"
 
 def register_images_endpoint(app: flask.Flask, semsearch_svc: service.SemSearchService) -> None:
     @app.route("/images", methods=["GET"])
-    def handler():
+    def images():
         payload = request.get_json()
 
         caption = payload[_CAPTION]
@@ -22,3 +22,11 @@ def register_images_endpoint(app: flask.Flask, semsearch_svc: service.SemSearchS
             return semsearch_svc.query_all_ims(caption, n=im_count)
 
         return semsearch_svc.query_labeled_ims(caption, n=im_count)
+
+
+def register_image_endpoint(app: flask.Flask, semsearch_svc: service.SemSearchService) -> None:
+    @app.route("/images/<im_id>")
+    def image(im_id):
+        im_path = semsearch_svc.get_im_path(im_id)
+
+        return flask.send_from_directory(im_path.parent, im_path.name)
