b819aff174d36fe8b3d77dd073c065720d2608dc
Ivan Hristov <ivan.hristov@siman.tech>
2024-05-01 15:32:13 +0300

    Rework REST API

--- dream/controller/restapi/__init__.py
+++ dream/controller/restapi/__init__.py
@@ -1,2 +1,6 @@
-from dream.controller.restapi.train import register_train_endpoint  # noqa
-from dream.controller.restapi.semsearch import register_images_endpoint, register_image_endpoint  # noqa
+from dream.controller.restapi.train import register_train_endpoint
+from dream.controller.restapi.semsearch import (
+    register_images_endpoint,
+    register_image_endpoint,
+    register_image_metadata_endpoint,
+)
--- dream/semsearch/store/store.py
+++ dream/semsearch/store/store.py
@@ -1,10 +1,12 @@
 import json
+import uuid
 from typing import Callable
 
 import psycopg_pool
 
 import dream.semsearch.service as semsearchservice
 from dream import model
+import dream.pg as dreampg
 
 
 class Store(semsearchservice.Store):
@@ -28,3 +30,15 @@ class Store(semsearchservice.Store):
             "INSERT INTO image_metadata (id, captions, dataset) VALUES(%s, %s, %s);",
             (im.id, persisted_captions, im.dataset),
         )
+
+    def get_image_metadata(self, tx: any, im_id: uuid.UUID) -> model.Image:
+        pg_tx = dreampg.to_tx(tx)
+
+        query = "SELECT captions, dataset FROM image_metadata im WHERE im.id = %s"
+        pg_tx.execute(query, (im_id,))
+
+        row = pg_tx.fetchone()
+        if row is None:
+            return None
+
+        return model.Image().with_id(im_id).with_dataset(row[1]).with_captions(row[0])
